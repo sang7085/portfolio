a3d433b272bc0f8fcd57dae133e7286fb6423244
parksanghyun <sang7085@gmail.com>
2025-08-19 16:35:22 +0900

    모바일 work 이미지 스크롤버그 수정

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,15 @@ const nextConfig = {
   },
   assetPrefix: '',
   basePath: '',
+
+  // 정적 호스팅 친화적: 모든 경로를 / 로 끝나게 → out/work/a/index.html 형태로 생성
+  trailingSlash: true,
+
   webpack(config) {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
-      use: [
-        {
-          loader: "raw-loader",
-        },
-      ],
+      use: [{ loader: 'raw-loader' }],
     });
     return config;
   },
